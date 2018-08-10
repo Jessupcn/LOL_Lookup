3395b16123af2d7be6544124dbc5b66a928b540e
Connor Jessup <cjessup@umich.edu>
2018-08-10 19:24:50 -0400

    REFAC: API call

--- Server/api/summoner.js
+++ Server/api/summoner.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const data = require('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=RGAPI-c8d6bc44-1ced-4cf6-aed5-5cd77e5d1479');
+const data = require('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=<devKey>');
 module.exports = router;
 
 router.get('/:name', (req, res, next) => {
