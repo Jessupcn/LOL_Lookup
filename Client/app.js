3a6ade1f26d86279f3d22a4ce31e1adb828227a1
Connor Jessup <cjessup@umich.edu>
2018-08-11 13:32:13 -0400

    ADDED: front end boilerplate (index.html, index.js, style.css, app.js

---
+++ Client/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Calendar } from './components';
+
+/*
+* Entry point into Components
+*/
+
+const App = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
