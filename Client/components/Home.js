8ce9305e1d35b840fa3f8e7bdf10cada5303ec85
Val Pilossof <valpil610@gmail.com>
2018-08-23 13:42:23 -0400

    ADD: search bar functionality now can look up any name

--- Client/components/Home.js
+++ Client/components/Home.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import SearchSummoner from './SearchSummoner';
-import { fetchSummoner } from '../redux_store';
 
 /*
 HOME COMPONENT
 */
 class Home extends Component {
   componentDidMount() {
-    this.props.loadSummoner();
+    // this.props.loadSummoner();
   }
   render() {
     return (
@@ -19,14 +18,9 @@ class Home extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  loadSummoner: () => {
-    const action = fetchSummoner('WolverineUM');
-    return dispatch(action);
-  }
-});
+const mapDispatchToProps = dispatch => ({});
 
 export default connect(
   null,
-  mapDispatchToProps
+  null
 )(Home);
