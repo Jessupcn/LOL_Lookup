9722c31e773d4025585cd21307022c8650c1d4de
Connor Jessup <cjessup@umich.edu>
2018-10-30 15:36:08 -0400

    ADDED: redux store for summonerMatches

---
+++ Client/redux_store/summonerMatches.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+/**
+ * ACTION TYPES
+ */
+const GET_SUMMONER_MATCHES = 'GET_SUMMONER_MATCHES';
+
+/**
+ * INITIAL STATE
+ */
+const defaultSummonerMatches = {};
+
+/**
+ * ACTION CREATORS
+ */
+export const getSummonerMatches = summonerMatches => ({
+  type: GET_SUMMONER_MATCHES,
+  summonerMatches
+});
+
+/**
+ * THUNK CREATORS
+ */
+
+export const fetchSummonerMatches = accountId => dispatch =>
+  axios
+    .get(`/api/summoners/${accountId}/matches`)
+    .then(res => res.data)
+    .then(summonerMatches => {
+      console.log('SUMMONER MATCHES: ', summonerMatches);
+      dispatch(getSummonerMatches(summonerMatches));
+    })
+    .catch(err => console.log(err));
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultSummonerMatches, action) {
+  switch (action.type) {
+    case GET_SUMMONER_MATCHES:
+      return action.summonerMatches;
+    default:
+      return state;
+  }
+}
